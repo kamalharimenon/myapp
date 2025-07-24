e161a034519bb923643b00928f2d88101f0a5b8d
kamalharimenon <kamalharimenon@gmail.com>
2025-07-24 10:42:57 +0000

    Fix web page

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,11 @@ import 'package:myapp/orchid_detail_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:myapp/services/firestore_service.dart'; // Import FirestoreService
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
